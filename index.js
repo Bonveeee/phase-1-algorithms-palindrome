10515b7d4837a78c132e493fcc5c75a1ec515320
bonveeee <bonochieng@gmail.com>
2022-06-17 15:54:49 +0300

    commit tree 94751fa7375918e39a0d0d1fbbc120a675a6edda

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const wordSplit = word.split('');
+  const wordPalindrome = wordSplit.reverse().join('');
+  const checkPalindrome = (wordPalindrome === word)? true : false;
+  return checkPalindrome;
 }
-
+console.log(isPalindrome('robot'))
+console.log(isPalindrome('racecar'))
 /* 
   Add your pseudocode here
 */
+//is palindrome(word)
+//word = "mama"
+// if reversestring === word
+// pass
+// iff reverstring != word 
+//fail
 
+//declare
 /*
   Add written explanation of your solution here
 */
+//declare function
+// set methods that will reverse word string
+// check whether criteria are met for palindrome
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
